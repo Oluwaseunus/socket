27cc33625be6f95e462401aa5be1987ed477e373
Oluwaseun <oluwaseunus@gmail.com>
2021-05-12 12:49:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ['GET', 'POST'],
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', socket => {
+  socket.onAny((eventName, ...args) => {
+    console.log({ eventName, args });
+    switch (eventName) {
+      case 'join':
+        const clients = io.engine.clientsCount;
+        if (clients > 1) {
+          io.local.emit('joined')
+          // socket.broadcast.emit('joined', args[0]);
+          // io.to(socket.id).emit('')
+        }
+        break;
+
+      default:
+        socket.broadcast.emit(eventName, ...args);
+        break;
+
+    }
+  });
+});
+
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
