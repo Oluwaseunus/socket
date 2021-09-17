635668f1efad6dc94ad11cbf94704ae9f3ce89d8
Oluwaseun <oluwaseunus@gmail.com>
2021-09-17 12:27:10 +0100

    feature: migrate to typescript and add force_mute functionality

---
+++ index.ts
@@ -0,0 +1,77 @@
+import http from 'http';
+import dotenv from 'dotenv';
+import express from 'express';
+import { Server, Socket } from 'socket.io';
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = process.env.PORT || 7500;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+interface Connection {
+  user: string;
+  socket: Socket;
+}
+
+interface ForceMutePaylod {
+  userId: string;
+  sender: string;
+}
+
+const allSockets: Connection[] = [];
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    const disconnectedSocketIndex = allSockets.findIndex(
+      (entry) => socket.id === entry.socket.id
+    );
+
+    if (disconnectedSocketIndex !== -1) {
+      const disconnectedSocket = allSockets[disconnectedSocketIndex];
+      allSockets.splice(disconnectedSocketIndex, 1);
+      io.local.emit('disconnected', disconnectedSocket.user);
+    }
+  });
+
+  socket.on('force_mute', ({ userId, sender }: ForceMutePaylod) => {
+    const connection = allSockets.find(({ user }) => user === userId);
+
+    if (connection) {
+      io.to(connection.socket.id).emit('force_mute', { sender });
+    }
+  });
+
+  socket.onAny((eventName, ...args) => {
+    switch (eventName) {
+      case 'join':
+      case 'user-reconnected':
+        const user = args[0];
+        allSockets.push({ user, socket });
+        const clients = io.engine.clientsCount;
+        if (clients > 1) {
+          io.local.emit('joined');
+        }
+        break;
+
+      default:
+        socket.broadcast.emit(eventName, ...args);
+        break;
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
